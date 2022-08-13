7f2099795a3e82e71e6a9b33efda983f804b8698
falentio <riv.kevinfalent.io@gmail.com>
2022-08-13 17:23:09 +0700

    nggih

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,17 +1,4 @@
-<script lang="ts">
-	import { onMount } from "svelte";
-	let text = "s";
-
-	onMount(() => {
-		setTimeout(() => (text = " :"), 8_000);
-		setTimeout(() => (text = " :)"), 11_000);
-		setTimeout(() => (text = " :("), 11 * 8 * 2005);
-		return () => {};
-	});
-</script>
-
-<section class="bg-zinc-900 p-4 text-gray-100 flex flex-col">
-	<span class="text-center">
-		Made With &#9829 For My Friend{text}
-	</span>
+<section class="bg-zinc-900 p-2 text-gray-100 flex flex-col text-center">
+	<span> Dibuat oleh Siswa XII MIPA 1</span>
+	<span class="text-center">&copy; 2022</span>
 </section>
