ff5de5d1cc2ad1567bfc4d838539b26b7c98c76f
falentio <riv.kevinfalent.io@gmail.com>
2022-08-30 01:08:19 +0700

    fix 404

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -4,7 +4,6 @@
 <div class="p-4">
 	<ul>
 		<li><a href="admin/login">login</a></li>
-		<li><a href="admin/homework">homework</a></li>
 		<li><a href="admin/homework/create">homework/create</a></li>
 	</ul>
 </div>
