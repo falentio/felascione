69c0bf4fd4b0dceef5174cfce89e654086d0bfdb
falentio <riv.kevinfalent.io@gmail.com>
2022-10-04 14:05:03 +0700

    meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,34 @@
 	import "@fontsource/abel/400.css";
 	import "@fontsource/cinzel/400.css";
 
+	import { MetaTags } from "svelte-meta-tags"
+
 	import { browser } from "$app/environment";
 	import Navbar from "$lib/components/Navbar.svelte";
 	import Footer from "$lib/components/Footer.svelte";
 	import LoginGuard from "$lib/components/LoginGuard.svelte";
 </script>
 
+<MetaTags
+	title="Felascione Website - Smanta XII MIPA 1"
+
+	openGraph={{
+		type: "website",
+		title: "Felascione",
+		description: "Website kelas XII MIPA 1 yang sederhana",
+		images: [{ url: "/favicon.jpg", alt: "Felascione Logo" }],
+	}}
+
+	twitter={{
+		cardType: "summary",
+		site: "@falentio_",
+		handle: "@falentio_",
+		title: "Felascione",
+		description: "Website kelas XII MIPA 1 yang sederhana",
+		image: "/favicon.jpg",
+		imageAlt: "Felascione Logo",
+	}}
+/>
 <Navbar />
 <main class="min-h-screen flex flex-col">
 	<div class="container mx-auto">
